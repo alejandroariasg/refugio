8f6b1a13a9e66b1aab028e9501f55bc942bd1fd7
Alejandro Arias Gallo <aarias409@gmail.com>
2020-01-13 21:04:44 -0500

    Create update and delete

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -7,6 +7,8 @@ from apps.mascota.models import Mascota
 def index(request):
 	return render(request, 'mascota/index.html')
 
+
+#Vistas basadas en funciones
 def mascota_view(request):
 	if request.method == 'POST':
 		form = MascotaForm(request.POST)
@@ -20,6 +22,24 @@ def mascota_view(request):
 	return render(request, 'mascota/mascota_form.html', {'form':form})
 
 def mascota_list(request):
-	mascota = Mascota.objects.all()
+	mascota = Mascota.objects.all().order_by('id')
 	contexto = {'mascotas':mascota}
 	return render(request, 'mascota/mascota_list.html', contexto)
+
+def mascota_edit(request, id_mascota):
+	mascota = Mascota.objects.get(id=id_mascota)
+	if request.method == 'GET':
+		form = MascotaForm(instance=mascota)
+	else:
+		form = MascotaForm(request.POST, instance=mascota)
+		if form.is_valid():
+			form.save()
+		return redirect('mascota:mascota_listar')
+	return render(request, 'mascota/mascota_form.html', {'form':form})
+
+def mascota_delete(request, id_mascota):
+	mascota = Mascota.objects.get(id=id_mascota)
+	if request.method == 'POST':
+		mascota.delete()
+		return redirect('mascota:mascota_listar')
+	return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
